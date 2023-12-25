b4605d8d8caa62e86504f853c3faff196f8f6bd1
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-12-25 21:01:04 +0900

    Add new commands to command palette

--- dot_config/wezterm/launch.lua
+++ dot_config/wezterm/launch.lua
@@ -1,18 +1,44 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local module = {}
-local launch_menu = {}
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	table.insert(launch_menu, {
-		label = "PowerShell",
-		args = { "pwsh.exe", "-NoLogo" },
-	})
-end
+wezterm.on("augment-command-palette", function(window, pane)
+	return {
+		{
+			brief = "Powershell Workspace",
+			icon = "cod_terminal_powershell",
+			action = act.SwitchToWorkspace({
+				name = "powershell",
+				spawn = {
+					args = { "pwsh.exe", "-NoLogo" },
+				},
+			}),
+		},
+		{
+			brief = "Msys2 Workspace",
+			icon = "cod_terminal_cmd",
+			action = act.SwitchToWorkspace({
+				name = "msys2",
+				spawn = {
+					args = { "msys2.cmd", "-clang64", "-defterm", "-no-start", "-use-full-path", "-shell", "zsh" },
+				},
+			}),
+		},
+		{
+			brief = "Manjaro Workspace",
+			icon = "linux_manjaro",
+			action = act.SwitchToWorkspace({
+				name = "manjaro",
+				spawn = {
+					args = { "wsl", "--distribution", "Manjaro" },
+				},
+			}),
+		},
+	}
+end)
 
 function module.apply_to_config(config)
 	config.default_prog = { "msys2.cmd", "-clang64", "-defterm", "-no-start", "-use-full-path", "-shell", "zsh" }
-
-	config.launch_menu = launch_menu
 end
 
 return module

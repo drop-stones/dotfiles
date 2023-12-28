b459ec3e6c1401a23e3db5206d49ff51659a9828
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-12-28 19:38:14 +0900

    Add commands to control pane

--- dot_config/wezterm/keybinding/commands.lua
+++ dot_config/wezterm/keybinding/commands.lua
@@ -1,6 +1,14 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local function ActiveUnlessAltScreen(window, pane, key, mods, callback)
+	if pane:is_alt_screen_active() then
+		window:perform_action(act.SendKey({ key = key, mods = mods }), pane)
+	else
+		window:perform_action(callback, pane)
+	end
+end
+
 return {
 	-- Copy
 	{ key = "c", mods = "ALT", action = act.CopyTo("Clipboard") },
@@ -12,14 +20,74 @@ return {
 	{ key = "f", mods = "ALT", action = act.ActivateCommandPalette },
 
 	-- CTRL + h: Move to left pane
-	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{
+		key = "h",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "h", "CTRL", act.ActivatePaneDirection("Left"))
+		end),
+	},
 
 	-- CTRL + l: Move to right pane
-	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+	{
+		key = "l",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "l", "CTRL", act.ActivatePaneDirection("Right"))
+		end),
+	},
 
 	-- CTRL + j: Move to down pane
-	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	{
+		key = "j",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "j", "CTRL", act.ActivatePaneDirection("Down"))
+		end),
+	},
 
 	-- CTRL + k: Move to up pane
-	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{
+		key = "k",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "k", "CTRL", act.ActivatePaneDirection("Up"))
+		end),
+	},
+
+	-- CTRL + LeftArrow: Resize pane to left
+	{
+		key = "LeftArrow",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "LeftArrow", "CTRL", act.AdjustPaneSize({ "Left", 2 }))
+		end),
+	},
+
+	-- CTRL + RightArrow: Resize pane to right
+	{
+		key = "RightArrow",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "RightArrow", "CTRL", act.AdjustPaneSize({ "Right", 2 }))
+		end),
+	},
+
+	-- CTRL + DownArrow: Resize pane to right
+	{
+		key = "DownArrow",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "DownArrow", "CTRL", act.AdjustPaneSize({ "Down", 2 }))
+		end),
+	},
+
+	-- CTRL + UpArrow: Resize pane to right
+	{
+		key = "UpArrow",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			ActiveUnlessAltScreen(window, pane, "UpArrow", "CTRL", act.AdjustPaneSize({ "Up", 2 }))
+		end),
+	},
 }

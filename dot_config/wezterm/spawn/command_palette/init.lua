436141dfa1efd2f217cd621e1d4a21cce46af1ae
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-20 12:53:41 +0900

    feat(wezterm): spawn a new tab according to the current workspace in toggleterm

--- dot_config/wezterm/spawn/command_palette/init.lua
+++ dot_config/wezterm/spawn/command_palette/init.lua
@@ -1,7 +1,18 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local utils = require("spawn.utils")
 local spawn_commands = require("spawn.spawn_commands")
 local private = require("spawn.command_palette.private")
+
+local function AppendSpawnCommand(spawn_command, ...)
+	local command = spawn_command or {}
+	local args = command.args or {}
+	for _, arg in ipairs({ ... }) do
+		table.insert(args, arg)
+	end
+	command.args = args
+	return command
+end
 
 local command_palette = {
 	{
@@ -36,11 +47,22 @@ local command_palette = {
 			spawn = spawn_commands["manjaro"],
 		}),
 	},
-	-- {
-	-- 	brief = "ToggleTerm Tab",
-	-- 	icon = "custom_vim",
-	-- 	action = module.get_spawn_command_according_to_workspace("msys2"),
-	-- },
+	{
+		brief = "ToggleTerm Tab",
+		icon = "custom_vim",
+		action = wezterm.action_callback(function(window, pane)
+			local workspace_name = window:active_workspace()
+			local spawn_command = spawn_commands[workspace_name] or spawn_commands["zsh"]
+			if spawn_command.args[1] == "zsh" or spawn_command.args[1] == "msys2.cmd" then
+				spawn_command = AppendSpawnCommand(spawn_command, "-c", "nvim -c 'ToggleTerm direction=tab'")
+			elseif spawn_command.args[1] == "wsl" then
+				spawn_command = AppendSpawnCommand(spawn_command, "--exec", "nvim -c 'ToggleTerm direction=tab'")
+				-- elseif spawn_command.args[1] == "pwsh" then
+				-- 	spawn_command = AppendSpawnCommand(spawn_command, "-Command", "\"nvim -c 'ToggleTerm direction=tab'\"")
+			end
+			window:perform_action(act.SpawnCommandInNewTab(spawn_command), pane)
+		end),
+	},
 }
 
 -- Load spawn/private.lua

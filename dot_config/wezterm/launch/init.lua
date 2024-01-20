dbb8571a343895a1de62ab2fb43e2b0ff7d2a28f
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-20 16:49:20 +0900

    feat(wezterm): support OS specific features

--- dot_config/wezterm/launch/init.lua
+++ dot_config/wezterm/launch/init.lua
@@ -1,19 +1,18 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 local spawn = require("spawn")
 local spawn_utils = require("spawn.utils")
 
 local module = {}
 
 wezterm.on("augment-command-palette", function(window, pane)
-  return spawn.get_spawn_commands_for_palette()
+	return spawn.get_spawn_commands_for_palette()
 end)
 
 function module.apply_to_config(config)
-{{ if eq .chezmoi.os "windows" }}
-	config.default_prog = spawn_utils.Msys2Commands()
-  config.wsl_domains = wezterm.default_wsl_domains()
-{{ end }}
+	if spawn_utils.IsWindows() then
+		config.default_prog = spawn_utils.Msys2Commands()
+		config.wsl_domains = wezterm.default_wsl_domains()
+	end
 end
 
 return module

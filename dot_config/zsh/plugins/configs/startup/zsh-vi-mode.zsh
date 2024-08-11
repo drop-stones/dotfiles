0a3b26a6965bc5468002ea26b255cc9362436f75
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-08-11 16:57:33 +0900

    fix(zsh-vi-mode): Suppress warnings

--- dot_config/zsh/plugins/configs/startup/zsh-vi-mode.zsh
+++ dot_config/zsh/plugins/configs/startup/zsh-vi-mode.zsh
@@ -1,3 +1,6 @@
 # Only changing the escape key to `jk` in insert mode, we still
 # keep using the default keybindings `^[` in other modes
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+
+# https://github.com/jeffreytse/zsh-vi-mode/issues/159
+setopt re_match_pcre

caa4813aa8a50b527028c2918530c65968520b71
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-06 15:16:20 +0900

    feat(zsh): Execute windows commands in windows filesystem on WSL2

---
+++ dot_config/zsh/configs/startup/wsl2.zsh
@@ -0,0 +1,42 @@
+kernel_release=$(uname -r 2> /dev/null)
+if [[ $kernel_release != *"WSL2"* ]]; then
+  return
+fi
+
+function isWinDir {
+  case $PWD/ in
+  /mnt/*) return $(true) ;;
+  *) return $(false) ;;
+  esac
+}
+
+cmds=(
+  # Version Control System
+  "git"
+  "lazygit"
+  # Editor
+  "nvim"
+  # Display files/directories
+  "bat"
+  "dust"
+  "eza"
+  # Search Files/Text
+  "fd"
+  "rg"
+  # Fuzzy Finder
+  "fzf"
+  # Command Runner
+  "just"
+  # Dotfiles Manager
+  "chezmoi"
+)
+
+for cmd in $cmds; do
+  function $cmd() {
+    if isWinDir; then
+      $0.exe $@
+    else
+      /usr/bin/$0 $@
+    fi
+  }
+done

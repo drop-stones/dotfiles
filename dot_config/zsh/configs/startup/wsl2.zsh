5e8fa989e1343c4e71993a0ac701db9243a79486
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-06 22:57:47 +0900

    feat(zsh): Setup all windows commands to call on WSL2

--- dot_config/zsh/configs/startup/wsl2.zsh
+++ dot_config/zsh/configs/startup/wsl2.zsh
@@ -1,40 +1,19 @@
-kernel_release=$(uname -r 2> /dev/null)
+# Do noghing if not WSL2
+kernel_release=$(uname -r 2>/dev/null)
 if [[ $kernel_release != *"WSL2"* ]]; then
   return
 fi
 
-function isWinDir {
-  case $PWD/ in
-  /mnt/*) return $(true) ;;
-  *) return $(false) ;;
-  esac
-}
-
-cmds=(
-  # Version Control System
-  "git"
-  "lazygit"
-  # Editor
-  "nvim"
-  # Display files/directories
-  "bat"
-  "dust"
-  "eza"
-  # Search Files/Text
-  "fd"
-  "rg"
-  # Fuzzy Finder
-  "fzf"
-  # Command Runner
-  "just"
-)
-
-for cmd in $cmds; do
-  function $cmd() {
-    if isWinDir; then
-      $0.exe $@
-    else
-      /usr/bin/$0 $@
-    fi
-  }
+# Call windows exe if in windows filesystem
+for cmd cmd_path in "${(@kv)commands}"; do
+  # Check the command exist in both windows and WSL2
+  if [[ $cmd_path != "/mnt/"* ]] && (( $+commands[$cmd.exe] )); then
+    function $cmd() {
+      if [[ $PWD == "/mnt/"* ]]; then
+          $0.exe $@
+      else
+        /usr/bin/$0 $@
+      fi
+    }
+  fi
 done

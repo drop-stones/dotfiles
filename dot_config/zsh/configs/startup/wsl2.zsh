6da8025153d070c77a9335a5104762c6a2503959
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-06 23:07:13 +0900

    feat(zsh): Create an exclude set to call WSL2 commands also in windows filesystem

--- dot_config/zsh/configs/startup/wsl2.zsh
+++ dot_config/zsh/configs/startup/wsl2.zsh
@@ -4,10 +4,14 @@ if [[ $kernel_release != *"WSL2"* ]]; then
   return
 fi
 
+typeset -A exclude_set=(
+  ["chezmoi"]="/usr/bin/chezmoi"
+)
+
 # Call windows exe if in windows filesystem
 for cmd cmd_path in "${(@kv)commands}"; do
   # Check the command exist in both windows and WSL2
-  if [[ $cmd_path != "/mnt/"* ]] && (( $+commands[$cmd.exe] )); then
+  if [[ $cmd_path != "/mnt/"* ]] && (( $+commands[$cmd.exe] )) && (( !$+exclude_set[$cmd] )); then
     function $cmd() {
       if [[ $PWD == "/mnt/"* ]]; then
           $0.exe $@

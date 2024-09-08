3e99f221beef62d07e52eeaee4b90c90a5df5053
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-08 15:21:10 +0900

    feat(fzf): Set default command

--- dot_config/zsh/configs/startup/fzf-defaults.zsh
+++ dot_config/zsh/configs/startup/fzf-defaults.zsh
@@ -1,3 +1,5 @@
 # Read config file
 # TODO: Add comment when ver 0.55.0 is released
 export FZF_DEFAULT_OPTS_FILE=~/.config/fzf/config
+
+export FZF_DEFAULT_COMMAND="fd --color=always --strip-cwd-prefix --type f --hidden --follow --exclude .git"

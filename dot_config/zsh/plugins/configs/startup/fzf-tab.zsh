9cb6c7ae7d73bbb0673d3c6334c7ec939c6719ea
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-18 20:51:05 +0900

    feat(fzf-tab): Switch fzf-preview commands according to PWD on WSL2

--- dot_config/zsh/plugins/configs/startup/fzf-tab.zsh
+++ dot_config/zsh/plugins/configs/startup/fzf-tab.zsh
@@ -2,8 +2,25 @@
 
 # set list-colors to enable filename colorizing
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
 # preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza -1 --color=always $realpath'
+kernel_release=$(uname -r 2>/dev/null)
+if [[ $kernel_release != *"WSL2"* ]]; then
+  zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+  zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza -1 --color=always $realpath'
+else
+  zstyle ':fzf-tab:complete:cd:*' fzf-preview \
+    'if [[ $PWD == "/mnt/"* ]]; then
+        eza.exe -1 --color=always $realpath
+    else
+        /usr/bin/eza -1 --color=always $realpath
+    fi'
+  zstyle ':fzf-tab:complete:z:*' fzf-preview \
+    'if [[ $PWD == "/mnt/"* ]]; then
+        eza.exe -1 --color=always $realpath
+    else
+        /usr/bin/eza -1 --color=always $realpath
+    fi'
+fi
 
 zstyle ':fzf-tab:*' fzf-min-height 8

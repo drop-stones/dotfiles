5e0de5fded4bc44aec156462a977fe4c16768d72
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-28 21:58:25 +0900

    feat(fzf): Set default command for WSL2

--- dot_config/zsh/configs/startup/fzf-defaults.zsh
+++ dot_config/zsh/configs/startup/fzf-defaults.zsh
@@ -2,4 +2,11 @@
 # TODO: Add comment when ver 0.55.0 is released
 export FZF_DEFAULT_OPTS_FILE=~/.config/fzf/config
 
-export FZF_DEFAULT_COMMAND="fd --color=always --strip-cwd-prefix --type f --hidden --follow --exclude .git"
+export FZF_DEFAULT_COMMAND="if [[ $(realpath $PWD) == "/mnt/"* ]]; then
+    command fd.exe --color=always --strip-cwd-prefix --type f --type d --hidden --follow --exclude .git --path-separator='/'
+  else
+    command fd --color=always --strip-cwd-prefix --type f --type d --hidden --follow --exclude .git --path-separator='/'
+  fi"
+
+# For WSL2
+export WSLENV=FZF_DEFAULT_COMMAND:$WSLENV

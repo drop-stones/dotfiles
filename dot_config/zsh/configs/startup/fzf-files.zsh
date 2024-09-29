20061f29030caae26aac0171ed51476455543954
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-09-29 20:10:48 +0900

    fix(fzf): Fix fd command in fzf-cd()

--- dot_config/zsh/configs/startup/fzf-files.zsh
+++ dot_config/zsh/configs/startup/fzf-files.zsh
@@ -1,6 +1,10 @@
 function fzf-cd() {
   local selected_dir
-  selected_dir=$(fd --type d --color=always . "$1" | fzf +m --height 50% --preview 'eza --git --icons --color=always --group-directories-first --tree {}')
+  if [[ $(realpath $PWD) == "/mnt/"* ]]; then
+    selected_dir=$(command fd.exe --type d --color=always --path-separator='/' . $1 | fzf +m --height 50% --preview 'command eza.exe --git --icons --color=always --group-directories-first --tree {}')
+  else
+    selected_dir=$(command fd --type d --color=always --path-separator='/' . $1 | fzf +m --height 50% --preview 'command eza --git --icons --color=always --group-directories-first --tree {}')
+  fi
   if [[ -n "$selected_dir" ]]; then
     # Change to the selected directory
     z "$selected_dir" || return 1

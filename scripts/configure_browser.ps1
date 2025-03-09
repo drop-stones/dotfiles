5f80b25318ca913c139cb010a42f4e07c290008a
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-09 20:26:03 +0900

    fix(surfingkeys): Fix install commands

--- scripts/configure_browser.ps1
+++ scripts/configure_browser.ps1
@@ -13,25 +13,27 @@ function Install-SurfingKeysConfig {
   Push-Location
   Set-Location "$HOME/.local/share/surfingkeys-config/"
 
-  if (npm install) {
+  if (& npm install) {
     Write-LogMessage -b "[install] " "npm packages"
   } else {
     Write-LogMessage -r "[error] " "failed to install npm packages"
   }
 
-  if (npm run install) {
+  if (& npm run install) {
     Write-LogMessage -b "[install] " "surfingkeys.js to $HOME\.config\surfingkeys.js"
   } else {
     Write-LogMessage -r "[error] " "failed to install surfingkeys.js"
   }
   
-  if (npm run install -- --vivaldi) {
+  $cmd_args = @("run", "install", "--", "--vivaldi")
+  & npm @cmd_args
+  if ($LASTEXITCODE -eq 0) {
     Write-LogMessage -b "[install] " "surfingkeys.vivaldi.js to $HOME\.config\surfingkeys.vivaldi.js"
   } else {
     Write-LogMessage -r "[error] " "failed to install surfingkeys.vivaldi.js"
   }
 
-  if (npm run configure-vivaldi) {
+  if (& npm run configure-vivaldi) {
     Write-LogMessage -b "[configure] " "vivaldi preferences"
   } else {
     Write-LogMessage -r "[error] " "failed to configure vivaldi preferences"

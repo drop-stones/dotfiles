ee17111aa16e20b071340ac03cf5714399453bc2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-29 19:44:25 +0900

    feat(bat): Add aliases to colorize help text

--- dot_config/zsh/configs/startup/alias.zsh
+++ dot_config/zsh/configs/startup/alias.zsh
@@ -9,6 +9,10 @@ alias ls="eza --git --icons --color=always --group-directories-first"
 # zoxide
 alias cd="z"
 
+# help
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
 chezmoi_update_all() {
   chezmoi state delete-bucket --bucket=scriptState
   chezmoi update

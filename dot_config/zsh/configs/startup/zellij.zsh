db8c0e22ec2dec491aefc33aaca08ce875d45da1
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-08-17 15:26:42 +0900

    feat(zellij): Add zellij environment variables to WSLENV

---
+++ dot_config/zsh/configs/startup/zellij.zsh
@@ -0,0 +1,7 @@
+# Do noghing if not WSL2
+kernel_release=$(uname -r 2>/dev/null)
+if [[ $kernel_release != *"WSL2"* ]]; then
+  return
+fi
+
+WSLENV=$WSLENV\:ZELLIJ/w\:ZELLIJ_SESSION_NAME/w\:ZELLIJ_PANE_ID/w

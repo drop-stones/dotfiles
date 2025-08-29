4b1e1e36612e78e30cf03a2a1abc7dcdbd630e08
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-08-29 18:26:46 +0900

    feat(abbr): abbr settings in fish

---
+++ dot_config/fish/conf.d/20-aliases.fish
@@ -0,0 +1,20 @@
+# git
+abbr --add ga git add
+abbr --add gb git branch
+abbr --add grb git rebase
+abbr --add gs git status
+abbr --add gst git stash
+abbr --add gsw git switch
+
+# lazygit
+abbr --add lg lazygit
+
+# eza
+abbr --add ls eza --icons --group-directories-first
+abbr --add ll eza --icons --group-directories-first --long
+abbr --add la eza --icons --group-directories-first --all
+abbr --add lla eza --icons --group-directories-first --long --all
+abbr --add lt eza --icons --group-directories-first --tree
+
+# bat
+abbr --add cat bat

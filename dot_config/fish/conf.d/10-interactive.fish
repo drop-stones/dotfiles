0a1cdf7ceb3a9d3bdc7e018aabdc899aad5c7a3d
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-08-29 18:26:30 +0900

    feat(vi-mode): enable vi-mode in fish

---
+++ dot_config/fish/conf.d/10-interactive.fish
@@ -0,0 +1,10 @@
+# exit if not interactive
+if not status is-interactive
+    return
+end
+
+##############################################
+#  Vi mode
+##############################################
+
+fish_vi_key_bindings

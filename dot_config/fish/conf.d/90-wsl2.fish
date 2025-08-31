d36b32a5c82258041df528cac86abd01c885bdbe
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-08-31 22:12:28 +0900

    feat(wslwrap.fish): 🐟🔀 auto-switch Linux/Windows executables in WSL2

--- dot_config/fish/conf.d/90-wsl2.fish
+++ dot_config/fish/conf.d/90-wsl2.fish
@@ -9,3 +9,15 @@ add_wslenv LS_COLORS/w
 
 # zellij
 add_wslenv ZELLIJ/w ZELLIJ_SESSION_NAME/w ZELLIJ_PANE_ID/w
+
+# wslwrap.fish
+## auto
+wslwrap register git
+wslwrap register rg
+wslwrap register fd --path-separator=/
+wslwrap register eza
+wslwrap register nvim
+wslwrap register cargo
+wslwrap register lazygit
+## windows
+wslwrap register wsl --mode windows

3b47a178ef43bc4b73aab1888f98f07b3cd26b39
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-08-31 11:47:11 +0900

    feat(tide): set TokyoNight colors to tide

--- dot_config/fish/conf.d/51-tide.fish
+++ dot_config/fish/conf.d/51-tide.fish
@@ -19,5 +19,44 @@ tide configure --auto \
     --icons='Few icons' \
     --transient=Yes
 
+# TokyoNight Color Palette
+set -l comment 565f89
+set -l red f7768e
+set -l green 9ece6a
+set -l cyan 7dcfff
+set -l purple 9d7cd8
+set -l blue 7aa2f7
+set -l orange ff9e64
+set -l yellow e0af68
+
+# character
+set -U tide_character_color $green
+set -U tide_character_color_failure $red
+
+# pwd
+set -U tide_pwd_color_anchors $cyan
+set -U tide_pwd_color_dirs $blue
+set -U tide_pwd_color_truncated_dirs $purple
+
+# git
+set -U tide_git_color_branch $green
+set -U tide_git_color_conflicted $red
+set -U tide_git_color_dirty $yellow
+set -U tide_git_color_operation $orange
+set -U tide_git_color_staged $yellow
+set -U tide_git_color_stash $purple
+set -U tide_git_color_untracked $blue
+set -U tide_git_color_upstream $green
+set -U tide_git_icon 
+set -U tide_git_truncation_length 36 # the length to truncate the branch name to
+set -U tide_git_truncation_strategy l # git branch truncation strategy: left
+
+# status
+set -U tide_status_color $green
+set -U tide_status_color_failure $red
+
 # mark as initialized
 set -U __tide_user_initialized true
+
+# Reload tide
+tide reload

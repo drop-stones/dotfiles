3885e93ea1d819ff09f84108329ad9bcc321f42e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-11 15:53:48 +0900

    chore(wezterm): remove wezterm config

--- dot_config/wezterm/appearance/create_private.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-
-local module = {}
-
-function module.apply_to_config(config)
-	-- Private config
-end
-
-return module
--- dot_config/wezterm/appearance/font.lua
+++
@@ -1,8 +0,0 @@
-local module = {}
-
-function module.apply_to_config(config)
-	-- font
-	config.font_size = 9.0
-end
-
-return module
